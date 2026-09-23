7b136adab4c1996b932cfc9c736b6365cb2d5340
Swaraj Ambawade <157837874+SwarajAmbawade@users.noreply.github.com>
2026-09-23 13:45:46 +0530

    Add files via upload

---
+++ Week2/script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.gallery img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightboxImg');
+const closeBtn = document.getElementById('closeBtn');
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target !== lightboxImg) {
+    lightbox.style.display = 'none';
+  }
+});
