625e3637f080f8c5b0f33fd1d435a2294a04ba21
Swaraj Ambawade <157837874+SwarajAmbawade@users.noreply.github.com>
2026-09-23 14:01:13 +0530

    Add files via upload

---
+++ Week3/script.js
@@ -0,0 +1,32 @@
+const apiKey = "3f2bff66ec9a6808ecf00a6e2436fd09";
+
+function getWeather() {
+  const city = document.getElementById("cityInput").value.trim();
+
+  if (!city) {
+    alert("Please enter a city name");
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log("Weather Data:", data);
+
+      if (data.cod !== 200) {
+        alert(data.message);
+        return;
+      }
+
+      document.getElementById("cityName").textContent = data.name;
+      document.getElementById("temperature").textContent = `${data.main.temp} °C`;
+      document.getElementById("condition").textContent = data.weather[0].description;
+      document.getElementById("humidity").textContent = `Humidity: ${data.main.humidity}%`;
+    })
+    .catch(error => {
+      console.error(error);
+      alert("Error fetching weather data");
+    });
+}
